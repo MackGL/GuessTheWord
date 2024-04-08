83409ceea90d970c15c92ace9814297955c40072
MackGL <mackennzie.gilbert1@outlook.com>
2024-04-08 15:09:37 -0500

    get a random word

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #GuessTheWord
+
+import random
+
 #opens the words txt and reads it. Splitlines creates strings based on the breaks of each thing
 with open("Words.txt") as w:
     word_list = w.read().splitlines()
 
+random_number = random.randint(0, len(word_list)-1)
+wordchoice = word_list[random_number]
 def GetWord(word):
     return 0
 
