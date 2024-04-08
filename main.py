afb39a33a45bfd12741b4fcd35df02e6ebafc4ed
MackGL <mackennzie.gilbert1@outlook.com>
2024-04-08 15:02:37 -0500

    Get txt file and read it

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #GuessTheWord
+#opens the words txt and reads it. Splitlines creates strings based on the breaks of each thing
+with open("Words.txt") as w:
+    word_list = w.read().splitlines()
+
+def GetWord(word):
+    return 0
+
+
+
+
