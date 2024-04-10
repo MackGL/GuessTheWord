4c9812d2390a76ce18ea439082ec11545f3f7e11
MackGL <mackennzie.gilbert1@outlook.com>
2024-04-10 15:13:10 -0500

    When you guess the same letter, it ends the code. Need to fix.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ with open("words.txt","r") as file:
     contents = file.read()
     words = contents.split()
     randomWord = random.choice(words)
+
 def word_displayed(randomWord, letters_guessed):
     display = ''
     for letter in randomWord:
@@ -13,26 +14,41 @@ def word_displayed(randomWord, letters_guessed):
             display += '_'
     return display
 
-
-
 def game():
     word = randomWord
     guesses = 4
     letters_guessed = []
 
     print("Welcome To Guess The Word!")
-    print("You have 4 guesses left.")
+    print("You have 4 guesses left.", end = " ")
+    print("Enter a letter!")
 
     while guesses > 0:
         print(word_displayed(randomWord, letters_guessed))
         guess = input("Guess a letter: ")
-        letters_guessed.append(guess)
-        print("Letters guessed: ", letters_guessed)
-
 
         if not guess.isalpha():
             print("————————————————————————————")
-            print("That's not a letter. (You cannot enter whole words")
+            print("That's not a letter. (You cannot enter words, numbers, or special characters)")
+        else:
+            if guess in letters_guessed:
+                return
+            elif guess not in letters_guessed:
+                letters_guessed.append(guess)
+            print("Letters guessed: ", letters_guessed)
+            if guess in word:
+                print("You have ", guesses, " guesses left.")
+            else:
+                guesses -= 1
+                print("You have ", guesses, "guesses left.")
+
+
+
+
+
+    if guesses == 0:
+        print("Game Over. You Lost.")
+
 
 
 game()
