0588ddd4d4934d2926dd333d6107b99b4e946484
MackGl <mackennzie.gilbert1@outlook.com>
2024-04-10 19:45:33 -0500

    Problem Fixed, not to just add play again, add one point for guessing correctly, and add more words!

--- main.py
+++ main.py
@@ -19,37 +19,52 @@ def game():
     guesses = 4
     letters_guessed = []
 
+    #welcomes the user
     print("Welcome To Guess The Word!")
-    print("You have 4 guesses left.", end = " ")
-    print("Enter a letter!")
+    print("Guess a letter you think could be in the word below.", end = " ")
+    print("You have 4 guesses left.")
 
+    #allows the player to input a letter, and displays word
     while guesses > 0:
         print(word_displayed(randomWord, letters_guessed))
-        guess = input("Guess a letter: ")
+        guess = input('Guess a letter: ')
 
+        #restricts guess to only a letter
         if not guess.isalpha():
             print("————————————————————————————")
             print("That's not a letter. (You cannot enter words, numbers, or special characters)")
         else:
+            #stops letter repeats in letter_guessed
             if guess in letters_guessed:
-                return
-            elif guess not in letters_guessed:
+                print("————————————————————————————")
+                print("You have already guessed that letter.")
+                guesses += 1
+            #puts letter guessed in letters_guessed list
+            else:
+                print("————————————————————————————")
                 letters_guessed.append(guess)
-            print("Letters guessed: ", letters_guessed)
+                print("Letters guessed: ", letters_guessed)
+
+                #displays number of guesses left
             if guess in word:
                 print("You have ", guesses, " guesses left.")
-            else:
+                #subtracts guess when answer is wrong
+            elif guess not in word:
                 guesses -= 1
-                print("You have ", guesses, "guesses left.")
+                print("Incorrect Guess.", end= ' ')
+                print("You have", guesses, "guesses left.")
 
+        if all(letter in letters_guessed for letter in word):
+            print("Congratulations, you won!")
+            break
 
-
-
+    if all(guesses in word in letters_guessed):
+        print("Congratulation, you won!")
 
     if guesses == 0:
-        print("Game Over. You Lost.")
+        print("————————————————————————————")
+        print("Game Over. You Lost. The word was:", word)
 
 
 
 game()
-
